4864e6a6187d8a111102c900e2d9c3ecbaf5bd72
APRENDIZ <APRENDIZ@BOGDAFCDMFSP071>
2024-08-01 11:54:10 -0500

    cajero

--- Program.cs
+++ Program.cs
@@ -8,9 +8,51 @@ namespace Cajero
 {
     internal class Program
     {
+
+        static int VerificarCuenta(int cuentav, int [,]cuentas)
+        {
+            for(int i = 0; i < cuentas.GetLength(0); i++) {
+                if (cuentas[i, 0] == cuentav)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("HOLA");
+            int[,] cuentas = new int[3,3];
+
+            cuentas[0, 0] = 1000;
+            cuentas[0, 1] = 1234;
+            cuentas[0, 2] = 50;
+
+            cuentas[1, 0] = 1001;
+            cuentas[1, 1] = 0000;
+            cuentas[1, 2] = 320;
+
+
+            cuentas[2, 0] = 1002;
+            cuentas[2, 1] = 4321;
+            cuentas[2, 2] = 200;
+
+            Console.WriteLine("Digite el numero de cuenta");
+            int nverificar = int.Parse(Console.ReadLine());
+
+            int x = 0;
+            while(x == 5)
+            {
+                Console.WriteLine("Digite la opcion");
+                int op = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+
+                }
+            }
+
+            
         }
     }
 }
+
